fd3dd54a424fbc14b5825e7af47d5cab4d206669
Matteo Vilucchio <35531309+mvilucchio@users.noreply.github.com>
2024-03-11 19:00:33 +0100

    cleaned a bit

--- integrals.py
+++ integrals.py
@@ -1,14 +1,17 @@
 import numpy as np
 from numba import njit
+from root_finding import brent_root_finder
 
+# gaussian integration
 r, w = np.polynomial.hermite.hermgauss(99)
 
 roots = np.sqrt(2) * np.array(r)
 weights = np.array(w) / np.sqrt(np.pi)
 
 
+# ---
 @njit()
-def beta_q_e(q, m, e,p):
+def beta_q_e(q, m, e, p):
     return -2 * e / (1 + m**p - q**p)
 
 
@@ -18,9 +21,9 @@ def compute_q_FP(m, q, h, p, e):
         weights
         * (
             np.tanh(
-                beta_q_e(q, m, e,p)
+                beta_q_e(q, m, e, p)
                 * (
-                    p * (0.5 * beta_q_e(q, m, e,p)) * m ** (p - 1)
+                    p * (0.5 * beta_q_e(q, m, e, p)) * m ** (p - 1)
                     + roots * np.sqrt(p * q ** (p - 1) / 2)
                     + h
                 )
@@ -30,22 +33,14 @@ def compute_q_FP(m, q, h, p, e):
     )
 
 
-# @njit()
-# def compute_m(m, J0, q, e):
-#    return np.sum(
-#        weights
-#        * np.tanh(beta_q_e(q, m, e,p) * (3 * J0 * m**2 + roots * np.sqrt(3 / 2) * q))
-#    )
-
-
 @njit()
 def compute_m_FP(m, q, h, p, e):
     return np.sum(
         weights
         * np.tanh(
-            beta_q_e(q, m, e,p)
+            beta_q_e(q, m, e, p)
             * (
-                p * (0.5 * beta_q_e(q, m, e,p)) * m ** (p - 1)
+                p * (0.5 * beta_q_e(q, m, e, p)) * m ** (p - 1)
                 + roots * np.sqrt(p * q ** (p - 1) / 2)
                 + h
             )
@@ -54,24 +49,8 @@ def compute_m_FP(m, q, h, p, e):
 
 
 @njit()
-def compute_m_q_fixedpoint_FP(J0, h, p, beta, blend=0.8, tol=1e-7):
-    err = 1.0
-    m = 0.5
-    q = 0.5
-
-    while err > tol:
-        q_new = compute_q_FP(m, J0, q, h, p, beta)
-        m_new = compute_m_FP(m, J0, m, h, p, beta)
-        err = max([abs(q_new - q), abs(m_new - m)])
-        m = blend * m_new + (1 - blend) * m
-        q = blend * q_new + (1 - blend) * q
-
-    return m, q
-
-
-@njit()
 def f_FP(m, q, h, p, e):
-    beta = beta_q_e(q, m, e,p)
+    beta = beta_q_e(q, m, e, p)
     J0 = beta / 2
     integral = np.sum(
         weights
@@ -100,7 +79,7 @@ def f_FP(m, q, h, p, e):
 
 @njit()
 def deltaf_FP(m, q, h, p, e):
-    beta = beta_q_e(q, m, e,p)
+    beta = beta_q_e(q, m, e, p)
     J0 = beta / 2
     integral = np.sum(
         weights
@@ -128,13 +107,13 @@ def deltaf_FP(m, q, h, p, e):
         )
         / (-beta)
         + h * m
-        - (0.25 * beta_q_e(0, 0, e,p) ** 2 + np.log(2)) / (-beta_q_e(0, 0, e,p))
+        - (0.25 * beta_q_e(0, 0, e, p) ** 2 + np.log(2)) / (-beta_q_e(0, 0, e, p))
     )
 
 
 @njit()
 def s_FP(m, q, h, p, e):
-    beta = beta_q_e(q, m, e,p)
+    beta = beta_q_e(q, m, e, p)
     J0 = beta / 2
     integral = np.sum(
         weights
@@ -180,19 +159,67 @@ def dAT_condition(q, m, h, beta, J0, p):
     return 1 - 0.5 * p * (p - 1) * beta**2 * q ** (p - 2) * integral
 
 
-# @njit()
-# def compute_s(m, q, e):
-#    beta = beta_q_e(q, m, e,p)
-#    J0 = beta / 2
-#    i = np.sum(
-#        weights
-#        * (
-#            np.log(
-#                2
-#                * np.cosh(
-#                    -2 * e * (3 * J0 * m**2 + roots * np.sqrt(3 / 2) * q) / (1 - q**3)
-#                )
-#            )
-#        )
-#    )
-#    return beta**2 * q**3 - 3 * beta * J0 * m**3 - 0.25 * beta**2 * (1 + 3 * q**2) + i
+@njit()
+def compute_h(h, m, q, p, e):
+    return compute_m_FP(m, q, h, p, e) - m
+
+
+# ---
+@njit()
+def fixed_points_h_q(m, e, p, blend=0.8, tol=1e-8, h_init=0.8, q_init=0.5):
+    err = 1e10
+    q = q_init
+    h = h_init
+
+    while err > 1e1 * tol:
+        h_new = brent_root_finder(
+            compute_h,
+            -20,
+            20,
+            tol,
+            tol,
+            1_000,
+            (m, q, p, e),
+        )
+        q_new = compute_q_FP(m, q, h_new, p, e)
+
+        err = max(abs(h_new - h), abs(q_new - q))
+        h = blend * h + (1 - blend) * h_new
+        q = blend * q + (1 - blend) * q_new
+
+    return h, q
+
+
+def multiple_empty_arrays(shape, n):
+    return [np.empty(shape) for _ in range(n)]
+
+
+def observables(shape):
+    return {
+        "delta_f": np.empty(shape),
+        "T": np.empty(shape),
+        "dAT": np.empty(shape),
+        "q": np.empty(shape),
+        "h": np.empty(shape),
+        "s": np.empty(shape),
+    }
+
+
+# @dataclass
+# class Observables:
+#     T: np.ndarray
+#     q: np.ndarray
+#     h: np.ndarray
+#     delta_f: np.ndarray
+#     s: np.ndarray
+#     dAT: np.ndarray
+
+# def observables(shape):
+#     return Observables(
+#         T=np.empty(shape),
+#         q=np.empty(shape),
+#         h=np.empty(shape),
+#         delta_f=np.empty(shape),
+#         s=np.empty(shape),
+#         dAT=np.empty(shape)
+#     )
