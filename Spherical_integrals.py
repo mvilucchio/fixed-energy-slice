0ea416fa7fc733b5d5a826fcc160538943c42563
Matteo Vilucchio <35531309+mvilucchio@users.noreply.github.com>
2024-03-18 10:35:51 +0100

    Update Spherical_integrals.py

--- Spherical_integrals.py
+++ Spherical_integrals.py
@@ -11,3 +11,31 @@ roots = np.sqrt(2) * np.array(r)
 weights = np.array(w) / np.sqrt(np.pi)
 
 
+@njit()
+def beta_q_e_spherical(q, m, e, p):
+    return -2 * e / (1 + m**p - q**p)
+
+
+@njit()
+def compute_q_FP_spherical(m, q, h, p, e):
+    return
+
+
+def free_energy_FP(q, m, beta, h, p, e):
+    return (
+        0.5 * beta**2 * (1 - q**p)
+        + beta**2 * h**2 * (1 - q)
+        + log1p(-q)
+        + (q - m**2) / (1 - q)
+        + 0.5 * beta**2 * m**p
+    )
+
+
+def test_fun_2(h, q, m, beta, p):
+    return 0.5 * beta**2 * p * q ** (p - 1) + beta**2 * h**2 - (q - m**2) / (1 - q) ** 2
+
+
+def compute_overlaps_spherical(m, p, beta):
+    q = 1 - (0.25 * p * beta**2 * m ** (p - 2)) ** 2
+
+    return q, h
