d12f6163fbe7f616e8dcc1de5be82ad8fd331ce3
Matteo Vilucchio <35531309+mvilucchio@users.noreply.github.com>
2025-02-18 10:03:02 +0100

    florent's method

--- Spherical_integrals_e.py
+++ Spherical_integrals_e.py
@@ -2,7 +2,7 @@ import numpy as np
 from numba import njit, vectorize
 from root_finding import brent_root_finder
 from scipy.optimize import root_scalar
-from math import log, log1p, exp, pi
+from math import log, log1p, exp, pi, sqrt
 
 
 @vectorize()
@@ -76,6 +76,16 @@ def compute_q_FP_J0(m, q, p, beta):
         m ** 2 +
         beta ** 2 * (1 - q) ** 2 * (0.5 * p * q ** (p - 1) )
     )
+
+@njit()
+def newm(thism, thisq, p, beta):
+    return sqrt(thisq - (1 - thisq) * (1 - thisq) * (0.5 * beta**2 * p * pow(thisq, p - 1)))
+
+
+@njit()
+def newq(thism, thisq, p, beta, J0):
+    return 1 - thism / (beta * p * J0 * pow(thism, p - 1))
+
 
 def fixed_points_q_J0(m, beta, p, blend=0.01, tol=1e-9, q_init=0.9):
     err = 1e10
